4839edc9287c1e5c631e853e9bf7d18c9ee32624
Neeraj Chauhan <ncilni@gmail.com>
2025-09-19 12:28:56 +0100

    Get endpoints created for Student Entity

--- src/main/java/com/neerajchauhan/springboot/RestSpringBootApi/controller/StudentController.java
+++ src/main/java/com/neerajchauhan/springboot/RestSpringBootApi/controller/StudentController.java
@@ -1,19 +1,28 @@
 package com.neerajchauhan.springboot.RestSpringBootApi.controller;
 
 import com.neerajchauhan.springboot.RestSpringBootApi.dto.StudentDto;
+import com.neerajchauhan.springboot.RestSpringBootApi.entity.Student;
+import com.neerajchauhan.springboot.RestSpringBootApi.repository.StudentRepository;
+import com.neerajchauhan.springboot.RestSpringBootApi.service.StudentService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class StudentController {
+import java.util.List;
 
-    @GetMapping("/student")
-    public StudentDto getStudent() {
-        return new StudentDto(  1L, "Neeraj", "ncilni@gmail.com");
+@RestController
+@RequiredArgsConstructor
+public class StudentController {
+    private final StudentService studentService;
+
+    @GetMapping("/students")
+    public List<StudentDto> getStudents() {
+        return studentService.getAllStudents();
     }
 
-    @GetMapping("/student/{id}")
-    public StudentDto getStudentById() {
-        return new StudentDto(  1L, "Neeraj", "ncilni@gmail.com");
+    @GetMapping("/students/{id}")
+    public StudentDto getStudentById(@PathVariable Long id) {
+        return studentService.getStudentById(id);
     }
 }
